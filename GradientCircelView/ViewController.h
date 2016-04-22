c0c0e6d882bfc6d686395df205b22c4cd40f40bd
龙伟 <longhongwei17@163.com>
2016-04-22 13:39:40 +0800

    Initial Commit

---
+++ GradientCircelView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientCircelView
+//
+//  Created by longhongwei on 16/4/22.
+//  Copyright © 2016年 longhongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
